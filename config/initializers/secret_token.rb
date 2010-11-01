1e703e3d376c74a59d015b48895411526b32e230
Martin Rumball <mrumball@kitech.uk.com>
2010-11-01 14:29:28 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdlService::Application.config.secret_token = '3a389c1f4753058145fecdb2b3e022b72e43d4426a3b7d760fa35c105f24194ad16253c43ff7cbc87597c555e9f2832b6080254578fdd7f9a4adbbd604b38516'
